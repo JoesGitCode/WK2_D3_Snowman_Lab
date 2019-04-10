d179e42317f4927364f9ee3b6725cc368664d307
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-10 12:13:26 +0100

    Added HiddenWord class

---
+++ hiddenword.rb
@@ -0,0 +1,12 @@
+class HiddenWord
+
+  attr_reader :word
+  attr_accessor :display
+
+  def initialize(word, display)
+    @word = word
+    @display = display
+  end
+
+
+end
--- specs/hiddenword_spec.rb
+++ specs/hiddenword_spec.rb
@@ -4,5 +4,21 @@ require_relative('../hiddenword.rb')
 
 class TestHiddenWord < MiniTest::Test
 
+  def setup()
+    @word1 = HiddenWord.new("a", "*")
+    @word2 = HiddenWord.new("gotcha", "******")
+  end
 
+  def test_what_is_the_word
+    assert_equal("a", @word1.word)
+  end
+
+  def test_what_is_the_display
+    assert_equal("*", @word1.display)
+  end
+
+  def test_display_can_change
+    @word2.display = "****h*"
+    assert_equal("****h*", @word2.display)
+  end
 end
